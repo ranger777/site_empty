5fe3dee3a5c32b113d176a0c807d7be3d2e624ce
ranger777 <81531428+ranger777@users.noreply.github.com>
2024-04-04 23:29:02 +0700

    Update myDB.py

--- myDB.py
+++ myDB.py
@@ -5,10 +5,10 @@ import sys
 class db_conn():
 
    def db_connected(self):
-      connected = psycopg2.connect(user="prokat54rf",
+      connected = psycopg2.connect(user="....",
                                    # пароль, который указали при установке PostgreSQL
-                                   password="QNPJw1i75zSE&u5A",
-                                   host="pg3.sweb.ru",
-                                   port="5432")
+                                   password="Q....",
+                                   host="....",
+                                   port="....")
       return connected
 
